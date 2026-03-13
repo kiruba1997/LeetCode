983e1f39842d0e9c37dafe25c3b773b40102e2ec
Kirubakaran Sivasankaran <121168027+kiruba1997@users.noreply.github.com>
2026-03-13 14:18:42 +0530

    Time: 3 ms (14.84%) | Memory: 43.7 MB (10.35%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -2,30 +2,28 @@ class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         Stack<Integer> st = new Stack<>();
         Queue<Integer> q = new LinkedList<>();
-        int n = sandwiches.length;
-        int m = students.length;
-        for(int i=n-1;i>=0;i--){
-            st.push(sandwiches[i]);
-        }
-        for(int i=0;i<m;i++){
+        for(int i=0;i<students.length;i++){
             q.add(students[i]);
         }
-        int count =0;
-        while(!st.isEmpty()){
-            if(st.isEmpty() && q.isEmpty()) return 0;
-            else if(st.peek().equals(q.peek())){
+        for(int i=sandwiches.length-1;i>=0;i--){
+            st.push(sandwiches[i]);
+        }
+        int count=0;
+        while(!q.isEmpty()){
+            if(st.peek().equals(q.peek())){
                 st.pop();
                 q.poll();
-                count =0;
+                count=0;
             }
             else{
                 q.add(q.poll());
                 count++;
-                if(count == st.size()){
+                if(count==q.size()){
                     return count;
                 }
             }
         }
         return q.size();
+
     }
 }
