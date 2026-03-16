9c0bfc03c3cf109289025be439599a07f23ba577
Kirubakaran Sivasankaran <121168027+kiruba1997@users.noreply.github.com>
2026-03-16 14:18:23 +0530

    Time: 5 ms (24.86%) | Memory: 46.6 MB (78.23%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,41 +1,43 @@
 class Solution {
-    List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
         char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
         }
-        generate(board, 0);
-        return res;
+        place(board,0,result);
+        return result;
     }
-    public void generate(char[][] board, int row) {
-        if (row == board.length) {
+    public void place(char[][] board,int row,List<List<String>> result){
+        if(row==board.length){
             List<String> list = new ArrayList<>();
-            for (int i = 0; i < board.length; i++) {
+            for(int i=0;i<board.length;i++){
                 list.add(new String(board[i]));
             }
-            res.add(list);
-            return;
+            result.add(list);
         }
-        for (int col = 0; col < board[row].length; col++) {
-            if (isSafe(board, row, col)) {
+        for(int col = 0;col<board.length;col++){
+            if(isSafe(board,row,col)){
                 board[row][col] = 'Q';
-                generate(board, row + 1);
-                board[row][col] = '.'; 
+                place(board,row+1,result);
+                board[row][col] = '.';
             }
         }
     }
-    public boolean isSafe(char[][] board, int row, int col) {
-    for(int i = 0; i < row; i++) {
-        if(board[i][col] == 'Q')
-            return false;
-        for(int j = 0; j < board.length; j++) {
-            if(board[i][j] == 'Q') {
-                if(Math.abs(row - i) == Math.abs(col - j))
-                    return false;
+
+    public boolean isSafe(char[][] board,int row, int col){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='Q'){
+                    if(Math.abs(row-i) == Math.abs(col-j)){
+                        return false;
+                    }
+                }
             }
         }
+        return true;
     }
-    return true;
-}
 }
