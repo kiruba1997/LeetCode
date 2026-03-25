3a67a26026747922e4bbdc34c3c2fda2ffd95ccf
Kirubakaran Sivasankaran <121168027+kiruba1997@users.noreply.github.com>
2026-03-25 11:35:02 +0530

    Time: 7 ms (60.55%) | Memory: 47.7 MB (5.66%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,37 @@
-import java.util.*;
-
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++) {
+        for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
-        }   
-        int[] indegree = new int[numCourses];   
-        for(int[] p : prerequisites) {
-            int a = p[0];
-            int b = p[1];
-            
-            adj.get(b).add(a); 
-            indegree[a]++;
         }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-                q.offer(i);
+        for(int[] edge:edges){
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int[] ind = new int[V];
+        for(int i=0;i<V;i++){
+            for(int n : adj.get(i)){
+                ind[n]++;
             }
         }
-        int count = 0;       
-        while(!q.isEmpty()) {
-            int node = q.poll();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(ind[i]==0){
+                q.add(i);
+            }
+        }
+        int count =0;
+        ArrayList<Integer> res= new ArrayList<>();
+        while(!q.isEmpty()){
+            int vertex = q.poll();
             count++;
-            
-            for(int nei : adj.get(node)) {
-                indegree[nei]--;
-                if(indegree[nei] == 0) {
-                    q.offer(nei);
+            res.add(vertex);
+            for(int nei:adj.get(vertex)){
+                ind[nei]--;
+                if(ind[nei]==0){
+                    q.add(nei);
                 }
             }
         }
-        return count == numCourses;
+        return count==V;
     }
 }
